42738ecf5a6cfb23c10f5f1d11c8fe5e0cebd653
rugbybread4free <rugbybread@gmail.com>
2020-08-18 12:08:56 +0200

    UI update 3 (render bugs solved/simplified, return function from list added)

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ import sys
 term = Terminal()
 
 def console_print(arg_string):
-    print(term.orange_on_black(arg_string))
+    print(term.orange(arg_string))
 
 
 def new_project():
@@ -22,32 +22,36 @@ def new_project():
     return
 
 def load_animation():
-    animation = ['|  | ', '/  / ', '-  - ', '\\  \\ ']
+    animation = ['|  |  | ', '/  /  /', '-  -  - ', '\\  \\  \\ ']
     anicount = 0
-    for i in range(0, 4):
-        time.sleep(1)
-        #sys.stdout.write("\r" + animation[anicount])
-        print(term.orange_on_black + animation[anicount], end="\r")
-        sys.stdout.flush()
-        anicount = (anicount + 1)
+    with term.hidden_cursor():
+        for i in range(0, 4):
+            time.sleep(0.5)
+            #sys.stdout.write("\r" + animation[anicount])
+
+            print(term.center(term.orange + animation[anicount]), end="\r")
+            sys.stdout.flush()
+            anicount = (anicount + 1)
 
 
 
 def timer(projectname):
     console_print("Start timer? y/n\n")
+
     if input() in ['y', 'Y']:
         start = time.time()
         data = 0
-        console_print("Press any key to stop timer: ")
+        print(term.center(term.orange + "Press any key to stop timer: "))
 
         with term.cbreak():
             val = ""
             while val.lower() == "":
-                val = term.inkey(timeout=0)
+                val = term.inkey(timeout=0.1)
                 if not val:
                     load_animation()
                 elif val:
-                    print(term.red_on_black + "Stopped timer!")
+                    print("\n")
+                    print(term.center(term.bright_white + "Stopped timer!"))
 
             end = time.time()
             returnvalue = datetime.timedelta(seconds=end-start)
@@ -64,13 +68,13 @@ def timer(projectname):
             savefile.truncate()
 
         try:
-            console_print("You spent {} seconds on {}".format(data, projectname))
-            print(term.on_black)
+            console_print("You spent {} seconds on {}\n".format(data, projectname))
+
 
         except NameError:
             print('NameError!')
     else:
-        return
+        pass
 
 
 def load_project():
@@ -78,7 +82,7 @@ def load_project():
         save_data = json.load(savefile)
 
 
-    chosen_project = list_projects()
+    chosen_project = list_projects_with_data()
 
     try:
         if chosen_project:
@@ -107,6 +111,41 @@ def list_projects():
             console_print(str(n) + '. ' + k)
             list_of_projects.append(k)
         while True:#Looping giving the user choice of project to load, until valid input is given
+            console_print('Select project by number (not listed key returns): ')
+            try:
+                choice_for_index = int(input()) - 1
+                choice_of_project = list_of_projects[choice_for_index]
+                return choice_of_project
+                break
+            except (IndexError, ValueError):
+                console_print("Returning... ")
+                return False
+
+
+def list_projects_with_data(): #Same base as list_projects(), but also lists the project data in a colum to the right
+    with open('save.json', 'r+') as savefile:
+        save_data = json.load(savefile)
+
+        n = 0
+        list_of_projects = []
+        print("\n", end="")
+        for k in save_data['projects'].keys():
+            n += 1
+            console_print(str(n) + '. ' + k)
+            list_of_projects.append(k)
+
+
+            print(term.rjust('Project name: ' + k))
+            print(term.rjust('Description: ' + save_data['projects'][k][
+                'description']))
+            total_elapsed_time = save_data['projects'][k]['total_elapsed_time']
+            print(term.rjust('Total time: ' + str(datetime.timedelta(seconds=total_elapsed_time))))
+
+
+
+
+
+        while True:#Looping giving the user choice of project to load, until valid input is given
             console_print('Select project by number: ')
             try:
                 choice_for_index = int(input()) - 1
@@ -119,27 +158,28 @@ def list_projects():
 
 
 
-
-
 def edit_projects():
     projectname = list_projects()
-    console_print('1. (D)elete project\n2. (R)eset time of project\n3. (E)dit time manually')
-    choice = input()
-    if choice in ['1', '1.', 'D', 'd', 'Delete', 'delete']:
-        delete_project(projectname)
+    if projectname == False:
         pass
-    elif choice in ['2', '2.', 'R', 'r', 'Reset', 'reset']:
-        reset_project(projectname)
-        pass
-    elif choice in ['3', '3.', 'E', 'e', 'Edit', 'edit']:
-        edit_time(projectname)
-        pass
+    elif True:
+        console_print('1. (D)elete project\n2. (R)eset time of project\n3. (E)dit time manually')
+        choice = input()
+        if choice in ['1', '1.', 'D', 'd', 'Delete', 'delete']:
+            delete_project(projectname)
+            pass
+        elif choice in ['2', '2.', 'R', 'r', 'Reset', 'reset']:
+            reset_project(projectname)
+            pass
+        elif choice in ['3', '3.', 'E', 'e', 'Edit', 'edit']:
+            edit_time(projectname)
+            pass
 
 
 def reset_project(projectname):
     with open('save.json', 'r+') as savefile:
         save_data = json.load(savefile)
-        print(term.white_on_black(('Reseting elapsed time in ' + str(projectname) +'...')))
+        print(term.bright_white(('Reseting elapsed time in ' + str(projectname) +'...')))
         save_data['projects'][projectname]['total_elapsed_time'] = 0
         savefile.seek(0)
         json.dump(save_data, savefile, indent=4)
@@ -151,7 +191,7 @@ def reset_project(projectname):
 def delete_project(projectname):
     with open('save.json', 'r+') as savefile:
         save_data = json.load(savefile)
-        print(term.white_on_black(('Deleting ' + str(projectname) + '...')))
+        print(term.bright_white(('Deleting ' + str(projectname) + '...')))
         print(term.clear_eol)
         del save_data['projects'][projectname]
         savefile.seek(0)
--- main.py
+++ main.py
@@ -41,14 +41,19 @@ def create_json():
         json.dump(data, datafile, indent=4)
 
 #create_json() #will recreate the save file - running this will erase existing data!
-
-
-
 #Beginning of program
-print(term.home + term.on_black + term.clear)
+
+
+Current_Date_Formatted = datetime.datetime.today().strftime ('%d%m%Y')
+
+print(term.home + term.clear)
+print(term.enter_fullscreen)
+print(term.move_y(term.height - 1))
+print(term.black_on_white(term.center('TaskTimer    ' + str(Current_Date_Formatted))))
+print(term.move_y(0))
+
 while True:
-
-
+    print(term.clear_eol)
     console_print("1. (N)ew project \n2. (L)oad existing project \n3. (P)roject options \n4. (Q)uit")
     choice = input()
     if choice in ['1', 'n', 'N', 'New', 'new']:
@@ -61,4 +66,5 @@ while True:
         edit_projects()
         pass
     elif choice in ['q', 'Q', 'quit', 'Quit', '4']:
+        print(term.exit_fullscreen)
         exit()
